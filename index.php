4d35ef9b9253469d0a862a762a8be7d700d673f4
DGalisanka <dgalisanka@me.com>
2017-02-12 19:53:32 +0200

    Knygu sarasas
    Knygu sarasas

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+$hostname = "mysql.hostinger.lt";
+$username = "u301441488_dg";
+$password = "labas1";			
+$dbname = "u301441488_books";
+
+$conn = new mysqli($hostname, $username, $password, $dbname);
+
+if ($conn->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+}
+
+mysqli_set_charset($conn, "utf8");
+
+$sql = "SELECT book_id, book_name FROM books";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Pavadinimas</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["book_id"]."</td><td><a href='page2.php?id=$row[book_id]'>".$row["book_name"]."</a></td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
---
+++ page2.php
@@ -0,0 +1,44 @@
+<?php
+
+	$hostname = "mysql.hostinger.lt";
+	$username = "u301441488_dg";
+	$password = "labas1";			
+	$dbname = "u301441488_books";
+
+	$book_id = $_GET['id'];
+
+	$conn = new mysqli($hostname, $username, $password, $dbname);
+
+	if ($conn->connect_error){
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	mysqli_set_charset($conn, "utf8");
+
+	$sql = "SELECT DISTINCT books.book_name, authors.author_name, genres.genre_name, release_dates.year FROM books LEFT JOIN  book_authors ON books.book_id = book_authors.book_id LEFT JOIN authors ON book_authors.author_id = authors.author_id LEFT JOIN book_genres ON books.book_id = book_genres.book_id LEFT JOIN genres ON book_genres.genre_id = genres.genre_id LEFT JOIN book_release_dates ON books.book_id = book_release_dates.book_id LEFT JOIN release_dates ON book_release_dates.release_date_id = release_dates.release_date_id WHERE books.book_id = $book_id";
+
+	$result = $conn->query($sql);
+
+	$book_names = array();
+	$author_names = array();
+	$genre_names = array();
+	$release_dates = array();
+
+	if ($result->num_rows > 0) {
+		echo "<table><tr><th>Pavadinimas</th><th>Autorius</th><th>Žanras</th><th>Leidimo metai</th></tr>";
+
+    	while($row = $result->fetch_assoc()) {
+        	array_push($book_names, $row['book_name']);
+        	array_push($author_names, $row['author_name']);
+        	array_push($genre_names, $row['genre_name']);
+        	array_push($release_dates, $row['year']);
+        }
+      	echo "<tr><td>".implode(", ", array_unique($book_names))."</td><td>".implode(", ", array_unique($author_names))."</td><td>".implode(", ", array_unique($genre_names))."</td><td>".implode(", ", array_unique($release_dates))."</td></tr>";  
+
+    	echo "</table>";
+	} else {
+    	echo "0 results";
+	}
+	$conn->close();
+
+?>
